e111bf941cfee34ad1243c2ffb484f46953844a9
Deandhito Rosewika Fahrudin Muhammad <dhitorfm@gmail.com>
2026-09-08 08:27:47 +0700

    thryje

--- src/Background.java
+++ src/Background.java
@@ -2,7 +2,6 @@ public class Background extends VisualAsset {
         private String name;
         public Background(String name, String imagePath) {
             super(imagePath);
-            this.name = name;
         }
         public String getName() { return name; }
     }
