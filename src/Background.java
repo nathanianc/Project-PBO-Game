2f2481993c48788d79f19b3dc9f49b9f6f928668
Deandhito Rosewika Fahrudin Muhammad <dhitorfm@gmail.com>
2026-09-08 08:29:36 +0700

    dasda

--- src/Background.java
+++ src/Background.java
@@ -2,6 +2,7 @@ public class Background extends VisualAsset {
         private String name;
         public Background(String name, String imagePath) {
             super(imagePath);
+            this.name = name;
         }
         public String getName() { return name; }
     }
